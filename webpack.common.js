5c618a6d55bbc206fa9dd593669db79a34f55def
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2024-01-30 12:58:17 -0500

    adding babel

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: './src/main.js',
+    main: "./src/main.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-
-      template: './src/index.html',
-      title: 'Template',
-      filename: 'index.html',
-      inject: 'body',
-
+      template: "./src/index.html",
+      title: "Template",
+      filename: "index.html",
+      inject: "body",
     }),
   ],
 
@@ -27,18 +25,29 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      //babel
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
